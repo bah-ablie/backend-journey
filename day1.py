d0d7735092e63e9ed18a6903c86687bbc77bd7d0
Abdoulie Bah <bahablie03@gmail.com>
2026-04-29 13:58:22 +0000

    Day 1 - setup and first python file

---
+++ day1.py
@@ -0,0 +1,12 @@
+# Day 1 - My Backend Journrey Begins
+print("I am becomming a Backend Developer")
+print("Today is Day 1. I will no quit")
+
+# Basic variables
+name = "Abdoulie Bha"
+goal = "Backend Developer"
+days_committed = 1
+
+print(f"My name is {name}")
+print(f"My goal is to become a {goal}")
+print(f"Days into the jourey: {days_committed}")
