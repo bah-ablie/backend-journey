dc4c44480677cca2cd045cb03ca8717268b51916
Abdoulie Bah <bahablie03@gmail.com>
2026-05-11 12:50:16 +0000

    Day 2 - Variables, Strings and numbers

---
+++ day2.py
@@ -0,0 +1,28 @@
+# Day 2 - Variables, Strings and numbers
+
+#---VARIABLES---
+my_name = "Abdoulie Bah"
+my_goal = "Backend Developer"
+current_day = 2
+
+# ---STRINGS---
+print(my_name.upper())
+print(my_name.lower())
+print(my_name.title())
+
+greeting = f"My name is {my_name} and I am on Day {current_day} of my journey"
+print(greeting)
+
+#---NUMBERS---
+age = 24
+daily_hours = 1.5
+days_in_roadmap = 112
+
+total_hours = daily_hours * days_in_roadmap
+print(f"if I say consistent, I will put in {total_hours} hours of learning")
+
+hobby = "Watching Movies"
+fav_book = "Atomic Habits"
+nickname = "Zap"
+message = f"My nickname is {nickname} and i like {hobby} in my free time and my favorite book is {fav_book}"
+print(message)
